713455f758c651af3d83ebb21bc0c9c50c7bfcc5
Vinicius Candido <vinicandido00@gmail.com>
2022-05-29 13:13:15 -0300

    First class: position

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,8 +1,12 @@
 package Application;
 
+import BoardGame.Position;
+
 public class Program {
     public static void main(String[] Args)
     {
+        Position position = new Position(2, 5);
 
+        System.out.println(position);
     }
 }
