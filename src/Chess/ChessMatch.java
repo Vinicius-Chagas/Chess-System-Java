9fe6fdfe251f0ac7f492aa30af9a62abd2c158d7
Vinicius Candido <vinicandido00@gmail.com>
2022-06-24 19:15:06 -0300

    Knight

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -5,6 +5,7 @@ import BoardGame.BoardException;
 import BoardGame.Piece;
 import BoardGame.Position;
 import Chess.Pieces.King;
+import Chess.Pieces.Knight;
 import Chess.Pieces.Rook;
 
 import java.util.ArrayList;
@@ -219,18 +220,8 @@ public class ChessMatch {
 
     private void initialSetup()
     {
-        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 1, new King(board, Color.WHITE));
+        placeNewPiece('c', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('c', 8, new Knight(board, Color.BLACK));
 
-        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 }
